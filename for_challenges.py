d47693f29fdef09986f6885b478f6307a7723ddd
Daria Veselkova <daria.veselkova@yandex.ru>
2021-11-28 18:17:55 +0300

    Tasks in 'for_challenges.py' done

--- for_challenges.py
+++ for_challenges.py
@@ -2,7 +2,8 @@
 # Необходимо вывести имена всех учеников из списка с новой строки
 
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for name in names:
+    print(name)
 
 
 # Задание 2
@@ -12,7 +13,8 @@ names = ['Оля', 'Петя', 'Вася', 'Маша']
 # Петя: 4
 
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for name in names:
+    print(f'{name}: {len(name)}')
 
 
 # Задание 3
@@ -25,7 +27,11 @@ is_male = {
     'Маша': False,
 }
 names = ['Оля', 'Петя', 'Вася', 'Маша']
-# ???
+for name in names:
+    if is_male[name]:
+        print(f'{name}, пол мужской')
+    else:
+        print(f'{name}, пол женский')
 
 
 # Задание 4
@@ -40,7 +46,11 @@ groups = [
     ['Вася', 'Маша', 'Саша', 'Женя'],
     ['Оля', 'Петя', 'Гриша'],
 ]
-# ???
+
+print(f'Всего {len(groups)} групп/-ы')
+
+for group in groups:
+    print(f'Группа {groups.index(group) + 1}: {len(group)} ученика/-ов')
 
 
 # Задание 5
@@ -54,4 +64,6 @@ groups = [
     ['Оля', 'Петя', 'Гриша'],
     ['Вася', 'Маша', 'Саша', 'Женя'],
 ]
-# ???
+
+for group in groups:
+    print(f"Группа {groups.index(group) + 1}: {', '.join(group)}")
